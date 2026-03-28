3fea438d05fe0be00fbe4eeb4546b28cae08fe66
belaayou <youssef.belaajel@gmail.com>
2026-03-28 15:13:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,33 @@
+// تعريف اسم الذاكرة التخزينية
+const CACHE_NAME = 'mgv-scan-cache-v1';
 
+// الملفات التي سيتم تخزينها للعمل بدون إنترنت
+const urlsToCache = [
+  './index.html',
+  './IMG_20260327_222846.jpg',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/html5-qrcode'
+];
+
+// خطوة التثبيت: حفظ الملفات في الكاش
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// خطوة التعامل مع الطلبات: تقديم النسخة المخزنة إن وجدت
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});

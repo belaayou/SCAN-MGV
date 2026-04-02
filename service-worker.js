b4dd4292b8fe87d9e3b406fac3c59fa708dffdd0
belaayou <youssef.belaajel@gmail.com>
2026-04-02 23:56:08 +0100

    Add service worker for asset caching
    Implement service worker for caching assets.

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,26 @@
+const CACHE_NAME = 'scan-industrial-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/html5-qrcode',
+  'https://cdn.tailwindcss.com'
+];
 
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((res) => {
+      return res || fetch(evt.request);
+    })
+  );
+});
+        

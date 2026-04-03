439a45a045516866937b5300652b9398ec0d72cd
belaayou <youssef.belaajel@gmail.com>
2026-04-03 20:37:26 +0100

    Update service worker cache name and URLs

--- sw.js
+++ sw.js
@@ -1,29 +1,33 @@
-const CACHE_NAME = 'mgv-scan-stellantis-v2';
-
+const CACHE_NAME = 'mgv-scan-cache-v1';
 const urlsToCache = [
+  './',
   './index.html',
-  './IMG_20260328_193404.png',
+  './manifest.json',
+  './IMG_20260328_193404.png.jpg',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/html5-qrcode'
 ];
 
+// Installation : Sauvegarde des fichiers
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Fetch : Utilisation des fichiers en mode Hors-ligne
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+// Activation : Nettoyage
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker Activé');
 });

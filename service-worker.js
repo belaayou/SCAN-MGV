89b6867e694e91fd00a3a26bf55d4cdb31c2e307
belaayou <youssef.belaajel@gmail.com>
2026-04-24 13:12:48 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -41,21 +41,4 @@ self.addEventListener('fetch', (event) => {
       });
     })
   );
-});self.addEventListener('fetch', (event) => {
-  // On laisse passer les requêtes Google Sheets (envoi de données)
-  if (event.request.url.includes('script.google.com')) {
-    return; 
-  }
-
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Retourne le fichier du cache s'il existe, sinon tente le réseau
-      return response || fetch(event.request).catch(() => {
-        // Si tout échoue (hors-ligne complet), on renvoie index.html
-        if (event.request.mode === 'navigate') {
-          return caches.match('./index.html');
-        }
-      });
-    })
-  );
 });
